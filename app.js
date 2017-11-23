33934d58683e82f54f223a5e9bd9e93ba16e54ee
Tyler  Kenney <tk1800kick@yahoo.com>
2017-11-22 19:58:51 -0500

    the first go

---
+++ app.js
@@ -0,0 +1,115 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// new stuff starts here
+var session = require('express-session');
+var flash = require('express-flash');
+var env = require('dotenv').config();
+
+const Client = require('pg').Client;
+const client = new Client({
+  connectionString: process.env.DATABASE_URL
+});
+client.connect(); //connect to database
+
+// javascript password encryption (https://www.npmjs.com/package/bcryptjs)
+var bcrypt = require('bcryptjs');
+//  authentication middleware
+var passport = require('passport');
+// authentication locally (not using passport-google, passport-twitter, passport-github...)
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy({
+    usernameField: 'username', // form field
+    passwordField: 'password'
+  },
+  function(username, password, done) {
+    client.query('SELECT * FROM users WHERE username = $1', [username], function(err, result) {
+      if (err) {
+        console.log("SQL error");
+        //next(err);
+        return done(null,false, {message: 'sql error'});
+      }
+      if (result.rows.length > 0) {
+        var matched = bcrypt.compareSync(password, result.rows[0].password);
+        if (matched) {
+          console.log("Successful login, ", result.rows[0]);
+          return done(null, result.rows[0]);
+        }
+      }
+      console.log("Bad username or password");
+      // returning to passport
+      // message is passport key
+      return done(null, false, {message: 'Bad username or password'});
+    });
+  })
+);
+
+// Store user information into session
+passport.serializeUser(function(user, done) {
+  //return done(null, user.id);
+  return done(null, user);
+});
+
+// Get user information out of session
+passport.deserializeUser(function(id, done) {
+  return done(null, id);
+});
+
+// Use the session middleware
+// configure session object to handle cookie
+// req.flash() requires sessions
+app.use(session({
+  secret: 'COMP335',
+  resave:false,
+  saveUninitialized: true,
+}));
+
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+// new stuff ends here
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/users.js
@@ -0,0 +1,115 @@
+var express = require('express');
+var router = express.Router();
+var userLoggedin = 0;
+
+var env = require('dotenv').config();
+const Client = require('pg').Client;
+const client = new Client({
+  connectionString: process.env.DATABASE_URL
+});
+client.connect(); //connect to database
+
+var passport = require('passport');
+var bcrypt = require('bcryptjs');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  //res.send('respond with a resource');
+  res.render('/', { user: req.user }); //display user.hbs
+});
+
+router.get('/logout', function(req, res){
+    req.logout(); //passport provide it
+    res.redirect('/'); // Successful. redirect to localhost:3000/users
+});
+
+function loggedIn(req, res, next) {
+
+  if (req.user) {
+    userLoggedin == true;
+    next(); // req.user exists, go to the next function (right after loggedIn)
+  } else {
+    res.redirect('/login'); // user doesn't exists redirect to localhost:3000/users/login
+  }
+}
+
+router.get('/profile',loggedIn, function(req, res){
+      // req.user: passport middleware adds "user" object to HTTP req object
+      res.render('profile', { person: req.user });
+});
+
+function notLoggedIn(req, res, next) {
+  if (!req.user) {
+    userLoggedin == false;
+    next();
+  } else {
+    res.redirect('/profile');
+  }
+}
+
+router.get('/login', notLoggedIn, function(req, res){
+    //success is set true in sign up page
+    //req.flash('error') is mapped to 'message' from passport middleware
+    res.render('login', {success:req.query.success, errorMessage: req.flash('error')});
+
+});
+
+router.post('/login',
+  // This is where authentication happens - app.js
+  // authentication locally (not using passport-google, passport-twitter, passport-github...)
+  passport.authenticate('local', { failureRedirect: 'login', failureFlash:true }),
+  function(req, res,next) {
+    res.redirect('/profile'); // Successful. redirect to localhost:3000/users/profile
+});
+
+router.get('/signup',function(req, res) {
+    // If logged in, go to profile page
+    if(req.user) {
+      return res.redirect('/profile');
+    }
+    res.render('signup'); // signup.hbs
+});
+// check if username has spaces, DB will whine about that
+function validUsername(username) {
+  var login = username.trim(); // remove spaces
+  return login !== '' && login.search(/ /) < 0;
+}
+
+function createUser(req, res, next){
+
+  var salt = bcrypt.genSaltSync(10);
+  var pwd = bcrypt.hashSync(req.body.password, salt);
+
+  client.query('INSERT INTO IO_users (username, password) VALUES($1, $2)', [req.body.username, pwd], function(err, result) {
+    if (err) {
+      console.log("unable to query INSERT");
+      return next(err); // throw error to error.hbs.
+    }
+    console.log("User creation is successful");
+    res.redirect('/login?success=true');
+  });
+}
+
+router.post('/signup', function(req, res, next) {
+  // Reject users
+  if (!validUsername(req.body.username)) {
+    return res.render('signup');
+  }
+
+  client.query('SELECT * FROM IO_users WHERE username=$1',[req.body.username], function(err,result){
+    if (err) {
+      console.log("sql error ");
+      next(err); // throw error to error.hbs.
+    }
+    else if (result.rows.length > 0) {
+      console.log("user exists");
+      res.render('signup', { errorMessage: "true" });
+    }
+    else {
+      console.log("no user with that name");
+      createUser(req, res, next);
+    }
+  });
+});
+
+module.exports = router;
